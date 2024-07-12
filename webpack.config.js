8544cf1b1b7de5cda3e8c42ae691eaaeb0e58b93
Ali Munzer Ibraheem <ali.sy.9777@gmail.com>
2024-07-12 15:47:43 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name]-[hash][ext]',
+        },
+      },
+      {
+        test: /\.html$/i,
+        use: 'html-loader',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: 'src/index.html' })],
+}
